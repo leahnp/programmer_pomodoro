3bff99742fbb89d9ea3c356f9225fd69e1b61bd4
Leah Petersen <leahnpetersen@gmail.com>
2018-01-19 13:42:05 -0500

    take in user input for work length, break length and iterations

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Programmer Pomodoro
 Pomodoro timer for programmers with break time exercise suggestions.
 
+What is a "Pomodoro"? The word pomodoro means tomato in Italian and pomodori is plural - tomatoes! The Pomodoro Technique is a time management method - learn more about it on (Wikipedia)[https://en.wikipedia.org/wiki/Pomodoro_Technique].
+
 # To Run
 $ Python pomodoro.py
 
@@ -14,10 +16,10 @@ Coming soon!
 
 # Definitions
 
-*Total work period*: the complete amount of time including all work periods and all break periods
+*Total Pomodori*: the complete amount of time including all work periods and all break periods
 
-	For example: if you work for 30 minutes and take 10 minute breaks for 4 iterations, your total work period is 150 minutes (no final break period).
+For example: if you work for 30 minutes and take 10 minute breaks for 4 iterations, your Total Pomodori is 150 minutes (no final break).
 
-*Work Period*: the amount of time you spend working in one work/break cycle
-*Break Period*: the amount of time you spend in a break in one work/break cycle
-*Work/break Cycle*: the amount of time to complete on work period and one break period
+*Work Sprint*: the amount of time you spend working in one work/break cycle
+*Break*: the amount of time you spend in a break in one work/break cycle
+*Pomodoro*: the amount of time to complete on work period and one break period
--- pomodoro.py
+++ pomodoro.py
@@ -1,13 +1,16 @@
 import time
 import os
+import re
 
-def pomodoro(work_len, break_len, iterations):
+# complete total work cycle
+# announce breaks and work sprints
+def pomodori(work_len, break_len, iterations):
 	# convert work_len to seconds
 	work_len_sec =  work_len * 60
 	break_len_sec = break_len * 60
 
 	for i in range(iterations):
-		print(i)
+		print("You are on pomodoro number " + str(i+1))
 		# start work period
 		print("Start working for " + str(work_len) + " minutes!")
 		# TODO add minute count
@@ -24,7 +27,28 @@ def pomodoro(work_len, break_len, iterations):
 		else:
 			break
 
+	# finish total work period
+	print("Pomodori complete!")
 	os.system('say "you are done, please close the computer and throw it out a window"')
 
-# pomodoro(10, 5, 2)
+
+# capture user input on work length, break length and numbers of cycles
+def user_input():
+	# TODO add a user introduction to the program
+	work_len = clean_input(raw_input("How long do you want to work for (in minutes): "))
+	break_len = clean_input(raw_input("How long do you want your breaks to be (in minutes): "))
+	iterations = clean_input(raw_input("How many work/break cycles do you want to complete: "))
+
+	print "Ok, you will work for " + str(work_len) + " mins then take a " + str(break_len) + " min break, " + str(iterations) + " times."
+
+	# TODO add a "ready to start? prompt"
+	raw_input("How long do you want your breaks to be (in minutes): ")
+	pomodori(int(work_len), int(break_len), int(iterations))
+
+# only allow numbers
+def clean_input(raw_work_len):
+	work_len = re.sub('[^0-9]','', raw_work_len)
+	return work_len
+
+user_input()
 
